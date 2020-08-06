aded346d9bc92a95cb45f72c421831b79f1852d8
viclafouch <victor.dlf@outlook.fr>
2020-08-06 16:53:24 +0200

    update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,144 +1,35 @@
 module.exports = {
-  "extends": [
-    "airbnb",
-    "prettier",
-    "prettier/react"
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 2020,
-    "ecmaFeatures": {
-      "impliedStrict": true,
-      "classes": true
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
+  parser: 'babel-eslint',
+  settings: {
+    react: {
+      version: 'detect'
     }
   },
-  "env": {
-    "browser": true,
-    "node": true
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
-  "rules": {
-    "no-debugger": 0,
-    "no-alert": 0,
-    "no-await-in-loop": 0,
-    "no-return-assign": [
-      "error",
-      "except-parens"
-    ],
-    "no-restricted-syntax": [
-      2,
-      "ForInStatement",
-      "LabeledStatement",
-      "WithStatement"
-    ],
-    "no-unused-vars": [
-      1,
+  rules: {
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        "ignoreSiblings": true,
-        "argsIgnorePattern": "res|next|^err"
+        semi: false,
+        singleQuote: true,
+        printWidth: 130,
+        trailingComma: 'none',
+        arrowParens: 'avoid'
       }
-    ],
-    "prefer-const": [
-      "error",
-      {
-        "destructuring": "all",
-      }
-    ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
-    ],
-    "no-unused-expressions": [
-      2,
-      {
-        "allowTaggedTemplates": true
-      }
-    ],
-    "no-param-reassign": [
-      2,
-      {
-        "props": false
-      }
-    ],
-    "no-console": 0,
-    "import/prefer-default-export": 0,
-    "import": 0,
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "react/prop-types": 0,
-    "react/display-name": 0,
-    "import/no-unresolved": 0,
-    "import/no-named-as-default": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "react/no-array-index-key": 0,
-    "react/react-in-jsx-scope": 0,
-    "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
-    "react/no-unescaped-entities": 0,
-    "jsx-a11y/accessible-emoji": 0,
-    "react/require-default-props": 0,
-    "react/jsx-filename-extension": [
-      1,
-      {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
-      }
-    ],
-    "radix": 0,
-    "no-shadow": [
-      2,
-      {
-        "hoist": "all",
-        "allow": [
-          "resolve",
-          "reject",
-          "done",
-          "next",
-          "err",
-          "error"
-        ]
-      }
-    ],
-    "quotes": [
-      2,
-      "single",
-      {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
-      }
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        "tabWidth": 2,
-        "printWidth": 130,
-        "semi": false,
-        "singleQuote": true
-      }
-    ],
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/label-has-for": 2,
-    "jsx-a11y/role-has-required-aria-props": 2,
-    "jsx-a11y/role-supports-aria-props": 2,
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
-      {
-        "aspects": [
-          "invalidHref"
-        ]
-      }
-    ],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    ]
   },
-  "plugins": [
-    "html",
-    "prettier",
-    "react-hooks"
-  ]
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  }
 }
