4c2c4d7b426abf9ef4de16a22f41685615e57745
viclafouch <victor.dlf@outlook.fr>
2020-08-21 10:54:40 +0200

    feat: simple-import-sort / printWidth to 80

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  plugins: ['react'],
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
+  plugins: ['react', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
   parser: 'babel-eslint',
   settings: {
     react: {
@@ -16,17 +21,44 @@ module.exports = {
   },
   rules: {
     'react/prop-types': 'off',
+    'simple-import-sort/sort': 'error',
     'prettier/prettier': [
       'error',
       {
         semi: false,
         singleQuote: true,
-        printWidth: 130,
+        printWidth: 80,
+        tabWidth: 2,
+        jsxSingleQuote: false,
         trailingComma: 'none',
         arrowParens: 'avoid'
       }
     ]
   },
+  overrides: [
+    {
+      files: ['*.jsx', '*.js'],
+      rules: {
+        'simple-import-sort/sort': [
+          'error',
+          {
+            groups: [
+              // Packages. `react` related packages come first.
+              // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+              ['^react', '^@?\\w'],
+              // Absolute imports and Relative imports.
+              [
+                '^(utils|services|hooks|hoc|types|contexts|dictionary|components)(/.*|$)',
+                '^\\.'
+              ],
+              // for scss imports.
+              ['^[^.]']
+            ]
+          }
+        ]
+      }
+    }
+  ],
   env: {
     browser: true,
     es6: true,
