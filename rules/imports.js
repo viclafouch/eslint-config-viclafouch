945dfa592631cde207bb55b883757b1fa8506194
viclafouch <victor.dlf@outlook.fr>
2023-07-11 11:40:10 +0200

    chore: add default simple-import-sort config

--- rules/imports.js
+++ rules/imports.js
@@ -7,11 +7,41 @@ module.exports = {
   },
   plugins: ['simple-import-sort', 'import'],
   rules: {
-    'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'import/first': 'error',
     'import/newline-after-import': 'error',
-    'import/no-duplicates': 'error'
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            // Anything that starts with react
+            // e.g: import { useState } from 'react'
+            // e.g: import { useFela } from 'react-fela'
+            '^react',
+            // Anything that starts with next
+            // e.g: import { useRouter } from 'next/router'
+            '^next',
+            // Anything that starts with a letter
+            // e.g: import Downshift from 'downshift'
+            '^[a-z]',
+            // Anything that starts with @
+            // e.g: import { yupResolver } from '@hookform/resolvers/yup'
+            '^@',
+            // Anything that starts with a dot
+            // e.g: import { matchIsDate } from './utils/date
+            '^\\.',
+            // Side effect imports from lib
+            // e.g: import 'react-toastify/dist/ReactToastify.css'
+            '^\\u0000',
+            // Side effect import that starts with a dot
+            // e.g: import './setup-config'
+            '^\\u0000\\.'
+          ]
+        ]
+      }
+    ]
   },
   settings: {
     'import/resolver': {
