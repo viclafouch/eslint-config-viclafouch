17ee230ebfca3d43461f306133270142f2ff3b5e
viclafouch <victor.dlf@outlook.fr>
2025-03-07 14:57:16 +0100

    chore: update deps

--- reset.d.ts
+++ reset.d.ts
@@ -1,5 +1,4 @@
 export * from '@total-typescript/ts-reset'
-
 declare global {
   // https://twitter.com/erikras/status/1673694889974833152
   // eslint-disable-next-line @typescript-eslint/ban-types
