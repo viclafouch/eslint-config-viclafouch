502ed18fdc0d371cd3d70216ff7d6550d6fa64c2
viclafouch <victor.dlf@outlook.fr>
2026-01-28 15:21:34 +0100

    chore: update rules

--- prettier.mjs
+++ prettier.mjs
@@ -1,6 +1,5 @@
 import { createRequire } from 'node:module'
 
-import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 const require = createRequire(import.meta.url)
@@ -11,11 +10,41 @@ const require = createRequire(import.meta.url)
 export default [
   {
     plugins: {
-      ...eslintConfigPrettier.plugins,
       prettier: eslintPluginPrettier
     },
     rules: {
-      ...eslintConfigPrettier.rules,
+      // Disable ESLint core rules that conflict with Prettier
+      // These rules are explicitly listed instead of using eslint-config-prettier
+      // to avoid disabling react/jsx-newline (Prettier doesn't handle blank lines in JSX)
+      'arrow-parens': 'off',
+      'arrow-spacing': 'off',
+      'dot-location': 'off',
+      'no-confusing-arrow': 'off',
+      'no-extra-semi': 'off',
+      'no-floating-decimal': 'off',
+      'no-multi-spaces': 'off',
+      'rest-spread-spacing': 'off',
+      'template-curly-spacing': 'off',
+
+      // Disable React JSX formatting rules that conflict with Prettier
+      // Note: react/jsx-newline is intentionally NOT disabled - Prettier doesn't handle it
+      'react/jsx-child-element-spacing': 'off',
+      'react/jsx-closing-bracket-location': 'off',
+      'react/jsx-closing-tag-location': 'off',
+      'react/jsx-curly-newline': 'off',
+      'react/jsx-curly-spacing': 'off',
+      'react/jsx-equals-spacing': 'off',
+      'react/jsx-first-prop-new-line': 'off',
+      'react/jsx-indent': 'off',
+      'react/jsx-indent-props': 'off',
+      'react/jsx-max-props-per-line': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'react/jsx-props-no-multi-spaces': 'off',
+      'react/jsx-tag-spacing': 'off',
+      'react/jsx-wrap-multilines': 'off',
+
+      // Run Prettier as an ESLint rule
+      // https://github.com/prettier/eslint-plugin-prettier
       'prettier/prettier': [
         'error',
         {
